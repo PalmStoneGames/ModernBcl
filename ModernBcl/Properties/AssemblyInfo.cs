ab0765310013af5c2c0ec23020621737bb0bed57
Luna Duclos <luna.duclos@palmstonegames.com>
2014-08-10 13:06:11 +0200

    Initial commit

---
+++ ModernBcl/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ModernBcl")]
+[assembly: AssemblyDescription("4.5 .NET runtime pack for use with the 3.5 Framework, allows usage of async/await, as well as most features from the 4.5 runtime while using 3.5")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Palm Stone Games")]
+[assembly: AssemblyProduct("ModernBcl")]
+[assembly: AssemblyCopyright("Copyright Palm Stone Games ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1cfdbaf0-c67b-404f-841c-0eea7f02f90b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,6 @@
+This package is a downport of the .NET 4.5 runtime of mono, which is MIT licensed, to run with .NET 3.5 runtime.
+This allows environments which are stuck with an old version of Microsoft .NET or Mono to use features such as async/await as well as most of what the 4.5 .NET framework has to offer
+
+As almost all of the code in this package was taken from the Mono project, which gracefully released all of it under an MIT license, we've licensed this downport under the MIT license as well, enjoy!
+
+Note: I've only downported the parts I'm personally interested in, however, pull requests downporting additional parts (including passing tests) are welcome and will be merged.
